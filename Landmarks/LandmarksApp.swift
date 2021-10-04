5b42c8c8c1ce40ff27ea68e42d7289bcebf97df3
rodriguesporan <rodriguesporan@gmail.com>
2021-10-04 18:59:36 -0300

    feat: initialized project

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by André Rodrigues on 04/10/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
