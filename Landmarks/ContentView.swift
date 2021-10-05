ca86c5d1b9f6b80869fa4b3211b82a85f9479f6d
rodriguesporan <rodriguesporan@gmail.com>
2021-10-04 22:46:00 -0300

    feat: create a custom image view

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,9 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Turtle Rock")
-            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-            .padding()
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+        .padding()
             
     }
 }
